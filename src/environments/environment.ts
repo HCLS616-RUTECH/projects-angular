e9f1fd0af508f8a44034c18c88112399e37abe85
Bondarev.A18 <bondarev.a18@wb.ru>
2023-01-15 16:00:54 +0300

    project init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'projects-angular',
+    appId: '1:100176783051:web:458cb2c3345dc01db09d4e',
+    databaseURL: 'https://projects-angular-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'projects-angular.appspot.com',
+    apiKey: 'AIzaSyAm8GCV0cqGFej4nuop7qdEO4JFH9k4UnA',
+    authDomain: 'projects-angular.firebaseapp.com',
+    messagingSenderId: '100176783051',
+    measurementId: 'G-GXT04DL7VP',
+  },
   production: false
 };
 
